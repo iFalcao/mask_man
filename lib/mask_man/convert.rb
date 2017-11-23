7dcfb68c1480af9edb7e1b8621ed2d4c8e32c3b4
Icaro Falcao <vbsfalcao@gmail.com>
2017-11-23 09:10:04 -0300

    Return empty string when nil is given to Convert

--- lib/mask_man/convert.rb
+++ lib/mask_man/convert.rb
@@ -1,7 +1,7 @@
 module MaskMan
   class Convert
     def self.rm_special(string)
-      return string.gsub(/[^0-9A-Za-z]/, '')
+      return string == nil ? '' : string.gsub(/[^0-9A-Za-z]/, '')
     end
   end
 end
--- spec/mask_man_spec.rb
+++ spec/mask_man_spec.rb
@@ -1,47 +1,57 @@
-require "spec_helper"
+require 'spec_helper'
 include MaskMan
 
 RSpec.describe MaskMan do
-  it "has a version number" do
+  it 'has a version number' do
     expect(MaskMan::VERSION).not_to be nil
   end
 
   describe 'Convert Module' do
     context 'when the params are valid' do
-      it "return telephone masked string without special characters" do
-        expect( Convert.rm_special("(71) 3325-2564") ).to eq("7133252564")
+      it 'return telephone masked string without special characters' do
+        expect( Convert.rm_special('(71) 3325-2564') ).to eq('7133252564')
       end
 
-      it "returns a correct string without any changes" do
-        expect( Convert.rm_special("7133252564") ).to eq("7133252564")
+      it 'returns a correct string without any changes' do
+        expect( Convert.rm_special('7133252564') ).to eq('7133252564')
+      end
+    end
+
+    context 'when the params are invalid' do
+      it 'return empty string when nil is passed' do
+        expect( Convert.rm_special nil ).to eq('')
+      end
+
+      it 'return empty string when an empty string is passed' do
+        expect( Convert.rm_special '' ).to eq('')
       end
     end
   end
 
   describe 'Validate Module' do
     context 'method has_special_chars?' do
-      it "return true when string contains special characters" do
-        expect( Validate.has_special_chars?("(71) 3325-2564") ).to eq(true)
+      it 'return true when string contains special characters' do
+        expect( Validate.has_special_chars?('(71) 3325-2564') ).to eq(true)
       end
 
-      it "return false when string does not contains special characters" do
-        expect( Validate.has_special_chars?("7133252564") ).to eq(false)
+      it 'return false when string does not contains special characters' do
+        expect( Validate.has_special_chars?('7133252564') ).to eq(false)
       end
     end
 
     context 'method only_asc?' do
-      it "return true when string contains only asc bytes" do
-        expect( Validate.only_asc?("(71) 3325-2564") ).to eq(true)
+      it 'return true when string contains only asc bytes' do
+        expect( Validate.only_asc?('(71) 3325-2564') ).to eq(true)
       end
 
-      it "return false when string does not contains only asc bytes" do
-        expect( Validate.only_asc?("aøbauhrhræoeuacå") ).to eq(false)
+      it 'return false when string does not contains only asc bytes' do
+        expect( Validate.only_asc?('aøbauhrhræoeuacå') ).to eq(false)
       end
     end
 
     context 'method count_special_chars?' do
-      it "return 5 when string contains 5 special_chars" do
-        expect( Validate.count_special_chars("()-*\\") ).to eq(5)
+      it 'return 5 when string contains 5 special_chars' do
+        expect( Validate.count_special_chars('()-*\\') ).to eq(5)
       end
     end
   end
